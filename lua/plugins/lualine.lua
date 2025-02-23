3c8588af1c83eab1f503993cbde668b65501a7a1
Nikita Skulkin <60268027+sklknn@users.noreply.github.com>
2025-02-23 23:07:23 +0300

    custom lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,52 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+            options = {
+                incons_enabled = true,
+                theme = 'palenight',
+                component_separators = { left = '', right = ''},
+                section_separators = { left = '', right = ''},
+                ignore_focus = {},
+                always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = true
+
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'fileformat','filetype'},
+                lualine_y = {
+                    {
+                        'diagnostics',
+                    
+                        sources = {'nvim_lsp', 'nvim_diagnostic', 'coc'},
+
+                        sections = { 'error', 'warn', 'info', 'hint' },
+
+                        symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
+                        colored = true,           -- Displays diagnostics status in color if set to true.
+                        update_in_insert = false, -- Update diagnostics in insert mode.
+                        always_visible = true,   -- Show diagnostics even if there are none.
+                    }
+                },
+                lualine_z = {'datetime'}
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
+    end
+}
