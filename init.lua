e57bc9b236c7f44eb3052e5dbb935d14f5938876
Nikita Skulkin <60268027+sklknn@users.noreply.github.com>
2025-02-23 22:21:22 +0300

    add neo-tree

--- init.lua
+++ init.lua
@@ -1,12 +1,21 @@
+-- Welcome to my config :)
 require("config.lazy")
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+require("vim-options")
+
+
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' } 
+
+
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+    -- write there all needed language parsers or use TSInstall !!!
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },  
+})
+
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,19 @@
+
+-- Tab options
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+-- Telescope options
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+--Neotree on loading
+vim.cmd("Neotree")
+--Toggle neotree on space-t
+vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>')
+
