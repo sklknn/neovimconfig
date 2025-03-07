cf76086cc4654939350623dbc4386049d053fac0
Nikita Skulkin <60268027+sklknn@users.noreply.github.com>
2025-03-07 19:17:52 +0300

    fix docker compose mistaken as yaml and throw errors

--- init.lua
+++ init.lua
@@ -6,12 +6,21 @@ require("vim-options")
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-	-- write there all needed language parsers or use TSInstall !!!
-	ensure_installed = { "lua" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
-	auto_install = true,
+    -- write there all needed language parsers or use TSInstall !!!
+    ensure_installed = { "lua" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    auto_install = true,
+})
+
+vim.filetype.add({
+    filename = {
+        ["docker-compose.yml"] = "yaml.docker-compose",
+        ["docker-compose.yaml"] = "yaml.docker-compose",
+        ["compose.yml"] = "yaml.docker-compose",
+        ["compose.yaml"] = "yaml.docker-compose",
+    },
 })
 
 --Neotree on loading
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,74 +1,82 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ansiblels",
-					"azure_pipelines_ls",
-					"bashls",
-					"cssls",
-					"docker_compose_language_service",
-					"dockerls",
-					"gopls",
-					"terraformls",
-					"tflint",
-					"yamlls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-	        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ansiblels",
+                    "azure_pipelines_ls",
+                    "bashls",
+                    "cssls",
+                    "docker_compose_language_service",
+                    "dockerls",
+                    "gopls",
+                    "terraformls",
+                    "tflint",
+                    "yamlls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-                capabilities = capabilities
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.ansiblels.setup({
-                capabilities = capabilities
+            lspconfig.ansiblels.setup({
+                capabilities = capabilities,
             })
-			lspconfig.azure_pipelines_ls.setup({
-                capabilities = capabilities
+            lspconfig.azure_pipelines_ls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.bashls.setup({
-                capabilities = capabilities
+            lspconfig.bashls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.cssls.setup({
-                capabilities = capabilities
+            lspconfig.cssls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.docker_compose_language_service.setup({
-                capabilities = capabilities
+            lspconfig.docker_compose_language_service.setup({
+                capabilities = capabilities,
             })
-			lspconfig.dockerls.setup({
-                capabilities = capabilities
+            lspconfig.dockerls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.gopls.setup({
-                capabilities = capabilities
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.terraformls.setup({
-                capabilities = capabilities
+            lspconfig.terraformls.setup({
+                capabilities = capabilities,
             })
-			lspconfig.tflint.setup({
-                capabilities = capabilities
+            lspconfig.tflint.setup({
+                capabilities = capabilities,
             })
-			lspconfig.yamlls.setup({
-                capabilities = capabilities
-            })
+            lspconfig.yamlls.setup {
+                capabilities = capabilities,
+                settings = {
+                    yaml = {
+                        schemas = {
+                            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+                            ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json"] = "/*.k8s.yaml",
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+                        },
+                    },
+                }
+            }
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
